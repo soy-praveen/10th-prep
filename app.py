1835f9fcf5ffad4a284130e723ed50d0c1f92570
Praveen <yosoypraveen@gmail.com>
2025-06-28 19:53:03 +0530

    km

--- app.py
+++ app.py
@@ -95,4 +95,6 @@ def biology():
     return render_template('under_construction.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
